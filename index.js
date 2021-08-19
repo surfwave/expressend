6ee19ecef0536334182efb6ce95f7d5c9191f158
surfwave <surfwave@gmail.com>
2021-08-19 16:39:09 +0800

    uibuild

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
 
 morgan.token('body', req => {
     return JSON.stringify(req.body);
 })
 
-app.use(cors());
+// app.use(cors());
+app.use(express.static('build'));
 app.use(express.json());
 app.use(morgan(`:method :url :status :res[content-length] - :response-time ms :body`))
 
